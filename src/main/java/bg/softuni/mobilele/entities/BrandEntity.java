12fa1f2a6e2c8e9c65db02e3b19e7e1b903c9061
Vladislav Todorov <vladislav.todorov@abv.bg>
2024-08-11 08:39:53 +0300

    Adding brands and models.

---
+++ src/main/java/bg/softuni/mobilele/entities/BrandEntity.java
@@ -0,0 +1,30 @@
+package bg.softuni.mobilele.entities;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "brands")
+public class BrandEntity extends BaseEntity{
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "BrandEntity{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
