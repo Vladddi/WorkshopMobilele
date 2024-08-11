6d5d0b1a44ba4e829ef979e2541d639f8f738709
Vladislav Todorov <vladislav.todorov@abv.bg>
2024-08-11 09:28:06 +0300

    Adding offers.

---
+++ src/main/java/bg/softuni/mobilele/entities/OfferEntity.java
@@ -0,0 +1,97 @@
+package bg.softuni.mobilele.entities;
+
+import bg.softuni.mobilele.entities.enums.EngineEnum;
+import bg.softuni.mobilele.entities.enums.TransmissionEnum;
+import jakarta.persistence.*;
+import org.springframework.ui.Model;
+
+@Entity
+@Table(name = "offers")
+public class OfferEntity extends BaseEntity {
+    @Enumerated(EnumType.STRING)
+    private EngineEnum engine;
+    private String imageUrl;
+    private int mileage;
+    private int price;
+    private int year;
+    @Enumerated(EnumType.STRING)
+    private TransmissionEnum transmission;
+    @ManyToOne
+    private ModelEntity model;
+
+    //TODO - seller
+
+
+    public EngineEnum getEngine() {
+        return engine;
+    }
+
+    public void setEngine(EngineEnum engine) {
+        this.engine = engine;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public TransmissionEnum getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(TransmissionEnum transmission) {
+        this.transmission = transmission;
+    }
+
+    public ModelEntity getModel() {
+        return model;
+    }
+
+    public void setModel(ModelEntity model) {
+        this.model = model;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferEntity{" +
+                "engine=" + engine +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", mileage=" + mileage +
+                ", price=" + price +
+                ", year=" + year +
+                ", transmission=" + transmission +
+                ", model=" + model +
+                ", id=" + id +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
+
