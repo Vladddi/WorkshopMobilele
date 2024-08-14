6a4299e6eed54875f152dca8fe8dfe08ffaff5e1
Vladislav Todorov <vladislav.todorov@abv.bg>
2024-08-14 10:29:52 +0300

    Start the implementation of the user login.

---
+++ src/main/java/bg/softuni/mobilele/service/UserService.java
@@ -0,0 +1,9 @@
+package bg.softuni.mobilele.service;
+
+public interface UserService {
+//**
+
+    boolean authenticate(String userName, String password);
+
+    void loginUser(String userName);
+}
---
+++ src/main/java/bg/softuni/mobilele/service/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package bg.softuni.mobilele.service.impl;
+
+import bg.softuni.mobilele.model.entities.UserEntity;
+import bg.softuni.mobilele.repository.UserRepository;
+import bg.softuni.mobilele.security.CurrentUser;
+import bg.softuni.mobilele.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private CurrentUser currentUser;
+
+    public UserServiceImpl(PasswordEncoder passwordEncoder,
+                           UserRepository userRepository,
+                           CurrentUser currentUser) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.currentUser = currentUser;
+    }
+
+    @Override
+    public boolean authenticate(String userName, String password) {
+
+        Optional<UserEntity> userEntityOpt = userRepository.findByUsername(userName);
+        if (userEntityOpt.isEmpty()) {
+            return false;
+        } else {
+            return passwordEncoder.matches(password, userEntityOpt.get().getPassword());
+        }
+    }
+
+    @Override
+    public void loginUser(String userName) {
+        currentUser.setAnonymous(false);
+        currentUser.setName(userName);
+    }
+}
