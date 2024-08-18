ac085f1a8471b49b2c140c159c3147167176f7e2
Vladislav Todorov <vladislav.todorov@abv.bg>
2024-08-18 14:48:29 +0300

    Starting the validation.

--- src/main/java/bg/softuni/mobilele/model/service/UserLoginServiceModel.java
+++ src/main/java/bg/softuni/mobilele/model/service/UserLoginServiceModel.java
@@ -1,9 +1,14 @@
 package bg.softuni.mobilele.model.service;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public class UserLoginServiceModel {
     private String username;
     private String password;
 
+    @NotNull
+    @Size(min = 2)
     public String getUsername() {
         return username;
     }
@@ -12,7 +17,8 @@ public class UserLoginServiceModel {
         this.username = username;
         return this;
     }
-
+    @NotNull
+    @Size(min = 3)
     public String getPassword() {
         return password;
     }
